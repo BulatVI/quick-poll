962b9dbe27591dbb5e0547c7e7838b0e90b5d53a
Булат <bulat1987@gmail.com>
2024-11-14 20:41:43 +0500

    add clientv3 for path /v3/...

---
+++ src/main/java/com/example/quick_poll/client/SortWrapper.java
@@ -0,0 +1,16 @@
+package com.example.quick_poll.client;
+
+import org.springframework.data.domain.Sort;
+
+public class SortWrapper {
+
+    private Sort sort;
+
+    public Sort getSort() {
+        return sort;
+    }
+    public void setSort(Sort sort) {
+        this.sort = sort;
+    }
+     
+}
--- src/main/java/com/example/quick_poll/repository/PollRepository.java
+++ src/main/java/com/example/quick_poll/repository/PollRepository.java
@@ -1,16 +1,8 @@
 package com.example.quick_poll.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.example.quick_poll.model.Poll;
 
 public interface PollRepository extends PagingAndSortingRepository<Poll, Long>{
-
-    void deleteById(Long pollId);
-
-    Optional<Poll> findById(Long pollId);
-
-    Iterable<Poll> findAll();
 }
